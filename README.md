003f8ec2a6336a3a622e873893e1d9c976a6c92d
Sebastian Urrego <sebastian.urrego@loanlogics.com>
2024-03-06 14:27:35 -0500

    Created the first bash file for the configuration and updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FirsTime-StartUpMachine
-A repository to store importante scripts for initial installation for a linux machine
+Following the Pragmatic programmer principales I decided to create a Script for the initial installation for a personal machine.
+
+For example: Your machine stopped working, so you have this repository that will install the basics for putting you in track.
---
+++ scripts/InitialScript.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Update the system packages
+sudo apt -y update
+# Install the available updates
+sudo apt -y upgrade
+
+# Install the basics
+sudo apt -y install git ssh wget curl unzip jq 
+
+# Install Python and Python extras
+python3 python-pip pipx 
+
+### Install Docker Engine
+# Add Docker's official GPG key:
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Start docker and enabled it 
+sudo service docker start # On WSL
+
+
+### AWS cli installation
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Delete the AWS packages
+rm -rf aws awscliv2.zip
+
+### AZURE cli installation
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
